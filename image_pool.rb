12cfb900d0abefdfe4ffda49e8f080bc9cd4f54f
Mike <zenkalia@gmail.com>
2013-02-05 19:13:23 -0800

    :bow: i should have written a script that kills white space...

--- image_pool.rb
+++ image_pool.rb
@@ -4,14 +4,14 @@ require 'gosu'
 class ImagePool
   @@image_files = {}
   @@image_texts = {}
-  
+
   def self.get_file(window, filename)
     if !@@image_files.has_key?(filename)
       @@image_files[filename] = Gosu::Image.new(window, filename)
     end
     @@image_files[filename]
   end
-  
+
   def self.get_text(window, text)
     if !@@image_texts.has_key?(text)
       lines = text.split("\n")
